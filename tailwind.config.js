db53a54b2c5aef17a32d78c3c7d61c8ffded89da
allan <allansaleh95@gmail.com>
2021-06-29 22:12:27 +0300

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: { enabled: true, content: ["./pages/**/*.jsx"] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
