c7a662a36fd2eaeae6b5a3f1858aae8e23ba5d3c
allan <allansaleh95@gmail.com>
2021-06-29 17:24:15 +0300

    added eslint with airbnb guidlines

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+/* eslint-disable react/react-in-jsx-scope */
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  // let name = 'Allan';
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -13,11 +16,14 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to
+          {' '}
+          <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing
+          {' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +64,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by
+          {' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
