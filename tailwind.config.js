1946c56d099436f1840454a79134dcad5bd6b66d
Daban <dabansadiq@icloud.com>
2021-07-01 19:37:29 +0300

    added make a pet happy component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: { enabled: true, content: ["./pages/**/*.jsx"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      }),
+
+      textColor: {
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      },
+    },
   },
   variants: {
     extend: {},
