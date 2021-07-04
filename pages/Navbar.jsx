eadb0e4157e9039d01360b08f977c087f97e7e2e
Daban <dabansadiq@icloud.com>
2021-07-05 00:12:11 +0300

    added styling to thenavbar

--- pages/Navbar.jsx
+++ pages/Navbar.jsx
@@ -6,44 +6,77 @@ const Navbar = () => {
   const img = [Image];
 
   return (
-    <nav className="flex flex-row justify-around">
+    <nav className="flex flex-row justify-between bg-primary  py-6 px-24 h-24 justify-items-center ">
       {" "}
-      <div>
+      <div className="w-16 h-16 flex-1">
         {" "}
         <Link href="./">
-          <img
-            className=" img-showcase rounded-lg"
-            src="./Logo.png"
-            alt="animal profile "
-          />
+          <img className="w-16 h-16 " src="./Logo.png" alt="animal profile " />
         </Link>{" "}
       </div>
-      <div className="flex flex-row justify-around">
+      <div className="flex flex-row justify-around items-center flex-1">
         {" "}
-        <div>
+        <div className="">
           <Link href="/">
-            <button type="button">Home</button>
+            <button
+              type="button"
+              className="mr-2.5 text-gray-400 hover:text-highlight"
+            >
+              Home
+            </button>
           </Link>
           <Link href="/">
-            <button type="button">About</button>
+            <button
+              type="button"
+              className="mr-3 text-gray-400 hover:text-highlight"
+            >
+              About
+            </button>
           </Link>
           <Link href="/">
-            <button type="button">Contact</button>
+            <button
+              type="button"
+              className="mr-3 text-gray-400 hover:text-highlight"
+            >
+              Contact
+            </button>
           </Link>
           <Link href="/">
-            <button type="button">Adopt</button>
+            <button
+              type="button"
+              className="mr-3 text-gray-400 hover:text-highlight"
+            >
+              Adopt
+            </button>
           </Link>
           <Link href="/">
-            <button type="button">Resources</button>
+            <button
+              type="button"
+              className="mr-3 text-gray-400  hover:text-highlight"
+            >
+              Resources
+            </button>
           </Link>
         </div>{" "}
-        <div>
+        <div className="pr-8">
           {" "}
           <Link href="/">
-            <button type="button"> sign up</button>
+            <button
+              type="button"
+              className="mr-3  w-28 h-8 text-highlight border-2 border-highlight rounded-lg "
+            >
+              {" "}
+              Login
+            </button>
           </Link>
           <Link href="/">
-            <button type="button"> sign in</button>
+            <button
+              type="button"
+              className="mr-3 w-28 h-8 text-primary bg-highlight rounded-lg "
+            >
+              {" "}
+              Sign up
+            </button>
           </Link>
         </div>
       </div>
