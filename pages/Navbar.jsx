01c8bd844f7160485c39eab2d30729da777505fa
Daban <dabansadiq@icloud.com>
2021-07-05 01:52:40 +0300

    Fixed img alt

--- pages/Navbar.jsx
+++ pages/Navbar.jsx
@@ -3,44 +3,38 @@ import Link from "next/link";
 import Navbutton from "./Navbutton";
 
 const Navbar = () => (
-  <nav className="flex flex-row  bg-primary  py-6 px-24 h-24 justify-items-center ">
-    {" "}
+  <nav className="flex flex-row bg-primary py-6 px-24 h-24 justify-items-center ">
     <div className="w-2/4 h-20 relative">
-      {" "}
       <Link href="./">
         <img
           className="w-16 h-16 absolute -top-2"
           src="./Logo.png"
-          alt="animal profile "
+          alt="Website logo "
         />
-      </Link>{" "}
+      </Link>
     </div>
-    <div className="flex flex-row justify-around items-center  w-2/4">
-      {" "}
+    <div className="flex flex-row justify-around items-center w-2/4">
       <div>
         <Navbutton name="Home" link="./" />
         <Navbutton name="About" link="./" />
         <Navbutton name="Contact" link="./" />
         <Navbutton name="Adopt" link="./" />
         <Navbutton name="Resources" link="./" />
-      </div>{" "}
+      </div>
       <div className="pr-8">
-        {" "}
-        <Link href="/">
+        <Link href="./">
           <button
             type="button"
-            className="mr-3.5  w-28 h-10 text-highlight border-2 border-highlight rounded-lg "
+            className="mr-3.5 w-28 h-10 text-highlight border-2 border-highlight rounded-lg "
           >
-            {" "}
             Login
           </button>
         </Link>
-        <Link href="/">
+        <Link href="./">
           <button
             type="button"
             className="mr-3 w-28 h-10 text-primary bg-highlight rounded-lg "
           >
-            {" "}
             Sign up
           </button>
         </Link>
