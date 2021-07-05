03c25c5e0eafd98743932f16beaa3293794d3063
Daban <dabansadiq@icloud.com>
2021-07-05 15:29:26 +0300

    applied all the requsted changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,24 +4,24 @@ import Navbutton from "./Navbutton";
 
 const Navbar = () => (
   <nav className="flex flex-row bg-primary py-6 px-24 h-24 justify-items-center ">
-    <div className="w-2/4 h-20 relative">
+    <div className="w-2/4 mr-40 h-20 relative">
       <Link href="./">
         <img
-          className="w-16 h-16 absolute -top-2"
+          className="w-16 h-16 absolute -top-2 cursor-pointer"
           src="./Logo.png"
           alt="Website logo "
         />
       </Link>
     </div>
     <div className="flex flex-row justify-around items-center w-2/4">
-      <div>
+      <div className="inline-block float-right whitespace-nowrap">
         <Navbutton name="Home" link="./" />
         <Navbutton name="About" link="./" />
         <Navbutton name="Contact" link="./" />
         <Navbutton name="Adopt" link="./" />
         <Navbutton name="Resources" link="./" />
       </div>
-      <div className="pr-8">
+      <div className="pr-8 whitespace-nowrap">
         <Link href="./">
           <button
             type="button"
--- components/Navbutton.jsx
+++ components/Navbutton.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
 
-const Navbutton = (props) => {
-  const { name } = props;
-  const { link } = props;
-  return (
-    <Link href={link}>
-      <button type="button" className="mr-3 text-gray-300 hover:text-highlight">
-        {name}
-      </button>
-    </Link>
-  );
-};
+const Navbutton = ({ name, link }) => (
+  <Link href={link}>
+    <button type="button" className="mr-3 text-gray-300 hover:text-highlight">
+      {name}
+    </button>
+  </Link>
+);
 Navbutton.propTypes = {
   name: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
