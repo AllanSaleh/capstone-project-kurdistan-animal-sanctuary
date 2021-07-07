f34cafad852f469141d62710954c49defe4b448b
Daban <dabansadiq@icloud.com>
2021-07-07 16:02:43 +0300

    created branch and components folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 module.exports = {
-  purge: { enabled: true, content: ["./pages/**/*.jsx"] },
+  purge: {
+    enabled: true,
+    content: ["./pages/**/*.jsx", "./components/**/*.{js,ts,jsx,tsx}"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+        darken: "#e6bf00",
+      }),
+
+      textColor: {
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      },
+      borderColor: (theme) => ({
+        ...theme("colors"),
+
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      }),
+    },
   },
   variants: {
     extend: {},
