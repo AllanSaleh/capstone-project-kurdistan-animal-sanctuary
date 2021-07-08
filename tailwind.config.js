e652007a16127dbc58f50f8722d349772cdbd497
Ahmad F.ameen <ahmad.f.ameen@gmail.com>
2021-07-09 02:53:07 +0300

    added active variant and some color themes in tailwind.confg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,46 @@
 module.exports = {
-  purge: { enabled: true, content: ["./pages/**/*.jsx"] },
+  purge: {
+    enabled: true,
+    content: ["./pages/**/*.jsx", "./components/**/*.{js,ts,jsx,tsx}"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+        darken: "#e6bf00",
+      }),
+
+      textColor: {
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      },
+      borderColor: (theme) => ({
+        ...theme("colors"),
+
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      }),
+      placeholderColor: (theme) => ({
+        ...theme("colors"),
+
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      }),
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"],
+      textColor: ["active"],
+      borderColor: ["active"],
+    },
   },
   plugins: [],
 };
