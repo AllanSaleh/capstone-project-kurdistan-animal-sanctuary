aba08b6f3996fb5233f7c567c8badc53488eedb4
Mousa Al-Jawaheri <76170542+mosatii@users.noreply.github.com>
2021-07-11 02:28:49 +0300

    Finised the top of the page and still working on the Carousel

---
+++ components/ResourcesCenter.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function ResourceCenter() {
+  return (
+    <div className="block">
+      <h1 className="font-extrabold text-center pt-16 pb-5 text-Quicksand text-3xl text-primary">
+        RESOURCES CENTER
+      </h1>
+      <p className="text-center p-6 text-secondary">
+        lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit
+        amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem
+        ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem
+        ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet
+        lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum
+        dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum
+        dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem
+        lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor
+        lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit
+        amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem
+        ipsum dolor lorem ipsum dolor sit amet lorem
+      </p>
+    </div>
+  );
+}
---
+++ pages/resources-center.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ResourcesCenter from "../components/ResourcesCenter";
+
+export default function TheResourcesCenter() {
+  return (
+    <>
+      <ResourcesCenter />
+    </>
+  );
+}
