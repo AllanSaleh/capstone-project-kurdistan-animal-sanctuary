949f9ddb614468ce2d082ccc39a5a140c66c28d3
oamaje <80586423+oamaje@users.noreply.github.com>
2021-07-11 14:10:08 +0300

    testing pushing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   purge: { enabled: true, content: ["./pages/**/*.jsx"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        background: "url('../public/bg.png')",
+      }),
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+        darken: "#E6BF00",
+      }),
+      textColor: {
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      },
+      borderColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      }),
+    },
   },
   variants: {
     extend: {},
