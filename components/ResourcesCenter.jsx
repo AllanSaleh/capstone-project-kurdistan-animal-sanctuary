adba9d820a272cb8551c3eb6731bd205be8779d2
Mousa Al-Jawaheri <76170542+mosatii@users.noreply.github.com>
2021-07-12 17:15:58 +0300

    I am done with this component, I used arrows instead of hover

--- components/ResourcesCenter.jsx
+++ components/ResourcesCenter.jsx
@@ -1,6 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Slider from "react-slick";
+
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+
+import shelter1 from "../public/assests-shelters/shelter 1.jpg";
+
+import shelter2 from "../public/assests-shelters/shelter 2.jpg";
+
+import shelter3 from "../public/assests-shelters/shelter 3.jpeg";
+
+import shelter4 from "../public/assests-shelters/shelter 4.jpg";
+
+import shelter5 from "../public/assests-shelters/shelter 5.jpg";
+
+import shelter6 from "../public/assests-shelters/shelter 6.jpg";
+
+import shelter7 from "../public/assests-shelters/shelter 7.jpg";
+
+import shelter8 from "../public/assests-shelters/shelter 8.jpg";
+
+
+
+let shelterImg = [
+  {
+    picture: shelter1,
+    about: "1.lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem loremipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum"
+  },
+    {
+    picture: shelter2,
+    about: "2.lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem loremipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum"
+
+    
+  },
+  {
+    picture: shelter3,
+    about: "3.lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem loremipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum"
+  
+    
+  },
+  {
+    picture: shelter4,
+    about: "4.lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem loremipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum"
+    
+  },
+  {
+    picture: shelter5,
+    about: "5.lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem loremipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum"
+    
+  },
+  {
+    picture: shelter6,
+    about: "6.lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem loremipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum"
+    
+  },
+  {
+    picture: shelter7,
+    about: "7.lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem loremipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum"
+    
+  },
+  {
+    picture: shelter8,
+    about: "8.lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem loremipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum"
+    
+  },
+]
+ 
+ 
 
 export default function ResourceCenter() {
+  const NextArrow = ({ onClick }) => (
+    <div className="arrow next" onClick={onClick}>
+      <FaArrowRight />
+    </div>
+  );
+
+  const PrevArrow = ({ onClick }) => (
+    <div className="arrow prev" onClick={onClick}>
+      <FaArrowLeft />
+    </div>
+  );
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) =>   setImageIndex(next) ,
+    
+  };
+ 
+  
   return (
     <div className="block">
       <h1 className="font-extrabold text-center pt-16 pb-5 text-Quicksand text-3xl text-primary">
@@ -18,7 +115,24 @@ export default function ResourceCenter() {
         lorem ipsum dolor sit amet lorem lorem ipsum dolor lorem ipsum dolor sit
         amet lorem lorem ipsum dolor lorem ipsum dolor sit amet lorem lorem
         ipsum dolor lorem ipsum dolor sit amet lorem
-      </p>
+      </p> 
+      <h1 className="font-bold text-center font-Quicksand   text-primary pt-6">Find Shelters We Work With</h1>
+{console.log(imageIndex)}
+      <Slider className="slider" {...settings}>
+        {shelterImg.map((img, idx) => (
+          <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+           
+          <img src={img.picture.src} alt={img} />
+           
+          </div>
+        ))}
+        
+
+      
+      </Slider>
+      <div className="text-center text-secondary font-Quicksand m-16 ">{shelterImg[imageIndex].about}
+      </div>
+
     </div>
   );
 }
