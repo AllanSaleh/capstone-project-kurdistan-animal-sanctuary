4e3399905b03e347602f8f8adecf60578d45acb4
oamaje <80586423+oamaje@users.noreply.github.com>
2021-07-12 16:45:37 +0300

    need to work on margin top

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,27 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: () => ({
+        background: "url('../public/bg.png')",
+      }),
       backgroundColor: (theme) => ({
         ...theme("colors"),
         primary: "#056676",
         secondary: "#A3D2CA",
         highlight: "#FFD400",
+        darken: "#E6BF00",
       }),
-
       textColor: {
         primary: "#056676",
         secondary: "#A3D2CA",
         highlight: "#FFD400",
       },
+      borderColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#056676",
+        secondary: "#A3D2CA",
+        highlight: "#FFD400",
+      }),
     },
   },
   variants: {
