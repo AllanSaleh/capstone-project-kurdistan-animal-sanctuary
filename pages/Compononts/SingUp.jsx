5b2e11ba34d4bd4f8196f456adca5d8005694c30
Zhinoo <Zhinosalam188@gmail.com>
2021-07-28 18:47:50 +0300

    Responsive SingUp

--- pages/Compononts/SingUp.jsx
+++ pages/Compononts/SingUp.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Signup = () => (
   <div className="bg-background h-screen bg-clip-content bg-no-repeat  bg-cover flex">
     <backgroundImage />
-    <div className="m-auto bg-secondary bg-opacity-80 max-w-lg rounded-xl text-center w-96 ">
+    <div className="mx-5 my-auto sm:m-auto bg-secondary bg-opacity-80 max-w-lg rounded-xl text-center w-96 ">
       <div className="w-80 m-auto ">
-        <p className="text-primary text-5xl text-center font-Quicksand p-10 font-bold">
+        <p className="text-primary text-4xl sm:text-5xl text-center font-Quicksand p-10 font-bold">
           Join Us
         </p>
         <div className="flex flex-col ">
@@ -16,19 +16,19 @@ const Signup = () => (
           />
           <input
             placeholder="   Password   "
-            className="border  mb-6 border-primary rounded  text-primary text-sm h-10 bg-secondary"
+            className="border  mb-6 mx-auto sm:mx-0 w-4/5 sm:w-full border-primary rounded  text-primary text-sm h-10 bg-secondary"
             type="password"
           />
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row ml-9 sm:ml-0">
           <input
             placeholder="   First Name   "
-            className="border border-primary rounded text-primary text-sm w-30 h-10 bg-secondary"
+            className="border border-primary rounded text-primary text-sm w-28 sm:w-30 h-10 bg-secondary"
           />
           <span className="mr-6" />
           <input
             placeholder="   Last Name   "
-            className="border mb-6 border-primary rounded text-primary text-sm w-30 h-10 bg-secondary"
+            className="border mb-6 border-primary rounded text-primary text-sm w-28 w-30 h-10 bg-secondary"
           />
         </div>
         <button
@@ -40,7 +40,7 @@ const Signup = () => (
       </div>
       <div className="text-center text-primary font-Quicksand text-sm font-bold">
         <h1>Or continue With</h1> <br />
-        <div className="flex flex-row items-center justify-center p-8">
+        <div className="flex flex-row items-center justify-center  pb-4 sm:p-8">
           <span>
             <a href="www.facebook.com">
               <img
