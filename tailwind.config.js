b8cb3c22517145891d72c9a6dbfb8325cba6626d
Daban <dabansadiq@icloud.com>
2021-07-29 21:58:58 +0300

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: () => ({
+        background: "url('../public/bg.png')",
+      }),
       backgroundColor: (theme) => ({
         ...theme("colors"),
         primary: "#056676",
         secondary: "#A3D2CA",
         highlight: "#FFD400",
-        darken: "#E6BF00",
+        darken: "#e6bf00",
       }),
       textColor: {
         primary: "#056676",
