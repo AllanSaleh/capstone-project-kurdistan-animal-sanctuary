baccd4cf0ccc3ed827ae89014a5a9fcd9dca109a
Mousa Al-Jawaheri <76170542+mosatiii@users.noreply.github.com>
2021-07-29 22:20:37 +0300

    Animal of week (#25)
    * Finished whyAdopt
    
    * Aniaml of the week done!
    
    * applied most of the requests
    
    * done
    
    Co-authored-by: Mousa Al-Jawaheri <76170542+mosatii@users.noreply.github.com>
    Co-authored-by: Daban <dabansadiq@icloud.com>
    Co-authored-by: allan <allansaleh95@gmail.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import WhyAdopt from "../components/WhyAdopt";
+import AnimalOfWeek from "../components/AnimalOfWeek";
 
 export default function Home() {
   // let name = 'Allan';
   return (
     <>
-      <WhyAdopt />
+      <AnimalOfWeek />
     </>
   );
 }
