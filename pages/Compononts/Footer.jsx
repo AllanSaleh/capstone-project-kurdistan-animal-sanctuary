ffdb84fdddf75894dd7adc70801cbbd65fe9584a
Zhinoo <Zhinosalam188@gmail.com>
2021-07-30 00:55:03 +0300

    Footer Done

--- pages/Compononts/Footer.jsx
+++ pages/Compononts/Footer.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
-
 export default function footer() {
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
-    <footer className="w-full h-96 pt-10 bg-secondary text-2xl sm:text-center bg-paw  bg-no-repeat  bg-cover font-Quicksand">
-      <div className="  mx-5 justify-center pl-72">
-        <div className="  w-40 inline-block relative ">
+    <footer className="w-full h-96 pt-10 bg-secondary text-2xl bg-paw  bg-no-repeat  bg-cover font-Quicksand">
+      <div className=" md:block-inline mx-5 justify-center mx-8 md:mx-0 md:pl-56 md:py-5">
+        <div className=" hidden w-20 md:h-40 md:w-40 md:inline-block relative ">
           <img
             src="/logo.png"
             alt="logo"
-            className="justify-self-end sm:auto m-auto w-36 h-36  "
+            className="justify-self-end m-auto w-20 h-20 md:w-36 md:h-36  "
           />
         </div>
-        <div className=" pt-5 pl-24 sm:auto inline-block">
+        <div className=" mb-5 md:mb-0 md:pt-5 md:pl-24 inline-block text-lg md:text-2xl">
           <h1 className=" text-primary font-bold font-Quicksand">About Us</h1>
-          <p className=" sm:text-primary font-Quicksand ">
+          <p className="  text-primary font-Quicksand ">
             Kurdistan Animal Sanctacy (KAS) <br />
             is NGO that saves <br />
             animals and looks for a loving
@@ -24,11 +23,11 @@ export default function footer() {
             <br />
           </p>
         </div>
-        <div className=" sm:pt-5 pl-36 inline-block">
+        <div className=" md:pt-5 md:pl-36 inline-block text-lg md:text-2xl">
           <h1 className=" text-primary font-bold font-Quicksand">
             Get in touch
           </h1>
-          <p className=" sm:text-primary font-Quicksand ">
+          <p className=" text-primary font-Quicksand ">
             info@kas.com <br />
             964 123 456 7890 <br />
             KRG, Erbil, Iraq
@@ -43,14 +42,14 @@ export default function footer() {
                   bgColor="#056676"
                 />
               </li>
-              <li>
+              <li className="pl-1">
                 <SocialIcon
                   url="https://www.instagram.com"
                   network="instagram"
                   bgColor="#056676"
                 />
               </li>
-              <li>
+              <li className="pl-1">
                 <SocialIcon
                   url="https://www.facebook.com"
                   network="facebook"
