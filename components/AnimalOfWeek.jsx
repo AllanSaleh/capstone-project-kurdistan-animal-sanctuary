bb4ab65dbcea6bf5379e3306ee6f4cd136d7b0ce
Daban <dabansadiq@icloud.com>
2021-07-29 21:52:53 +0300

    applied most of the requests

--- components/AnimalOfWeek.jsx
+++ components/AnimalOfWeek.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 
-import Image from "next/image";
-
-import catPhoto from "../public/Group 790.png";
+import Link from "next/link";
 
 export default function AnimalOfWeek() {
   return (
     <div
       style={{ backgroundImage: "url(https://i.ibb.co/zGrMtLL/paw.png)" }}
-      className=" bg-no-repeat  bgSize	"
+      className=" bg-no-repeat  bg-contain 	"
     >
-      <div className="block p-7">
-        <h1 className="text-center text-primary text-3xl font-Quicksand font-bold">
+      <div className="block p-7 px-4 md:px-24">
+        <h1 className="pb-8 text-center text-primary text-3xl font-Quicksand font-bold">
           Animal of the week
         </h1>
-        <div className="flex">
-          <div className="w-9/12">
-            <Image src={catPhoto} />
+        <div className="block md:flex">
+          <div className="w-36 md:w-44 h-36 md:h-44 rounded-full mx-auto   ">
+            <Link href="./">
+              <img
+                className="  w-32 md:w-36 h-32 md:h-36 border-2 border-highlight p-2 rounded-full cursor-pointer"
+                src="./Group 790.png"
+                alt="animal profile "
+              />
+            </Link>
           </div>
-          <p className="text-center font-Quicksand">
+          <p className=" mx-auto md:mx-0  font-Quicksand text-justify w-10/12">
             lorem ipsum dolor sit amet lorem, consectetur adipiscing elits
             lorem. lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem,
             consectetur adipiscing elits lorem. lorem ipsum dolor sit amet lorem
@@ -29,14 +33,6 @@ export default function AnimalOfWeek() {
             lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem,
             consectetur adipiscing elits lorem. lorem ipsum dolor sit amet lorem
             ipsum dolor sit amet lorem, consectetur adipiscing elits lorem.
-            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem,
-            consectetur adipiscing elits lorem. lorem ipsum dolor sit amet lorem
-            ipsum dolor sit amet lorem, consectetur adipiscing elits lorem.
-            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem,
-            consectetur adipiscing elits lorem. lorem ipsum dolor sit amet lorem
-            ipsum dolor sit amet lorem, consectetur adipiscing elits lorem.
-            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem,
-            consectetur adipiscing elits lorem. lorem ipsum dolor sit amet
           </p>
         </div>
       </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-import WhyAdopt from "../components/WhyAdopt";
-
 import AnimalOfWeek from "../components/AnimalOfWeek";
 
 export default function Home() {
   // let name = 'Allan';
   return (
     <>
-      <WhyAdopt />
       <AnimalOfWeek />
     </>
   );
