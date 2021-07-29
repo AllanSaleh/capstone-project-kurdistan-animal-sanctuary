575ec67da02a89cdb9d18ecc4b38f36b9264bd67
Daban <dabansadiq@icloud.com>
2021-07-29 21:13:35 +0300

    applied requested changes

--- components/WhyAdopt.jsx
+++ components/WhyAdopt.jsx
@@ -3,13 +3,14 @@ import React from "react";
 export default function whyAdopt() {
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
-    <div className="bg-secondary">
-      <h1 className="text-center text-primary text-3xl font-bold font-Quicksand ">
+    <div className="bg-secondary py-6">
+      <h1 className="text-center text-primary text-4xl font-bold font-Quicksand ">
         Why Adopt?
       </h1>
-      <p className="text-center p-6 text-primary px-72 font-Quicksand ">
+      <p className="text-center p-6  text-lg text-primary md:px-64 font-Quicksand ">
         Here we will write a bit about what we do and why and the benfit of
-        adopting animals and helping them
+        adopting animals and helping them, Here we will write a bit about what
+        we do and why and the benfit of adopting animals and helping them
       </p>
     </div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
 import React from "react";
-import styles from "../styles/Home.module.css";
+
 import WhyAdopt from "../components/WhyAdopt";
 
 export default function Home() {
