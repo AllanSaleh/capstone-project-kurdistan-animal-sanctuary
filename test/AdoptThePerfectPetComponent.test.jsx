8ff30eca96ab2339a33d7ebc5055a4fd1934458b
Daban <dabansadiq@icloud.com>
2021-08-01 17:39:17 +0300

    fixed test

--- test/AdoptThePerfectPetComponent.test.jsx
+++ test/AdoptThePerfectPetComponent.test.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { test, expect } from "@jest/globals";
 import AdoptThePerfectPetComponent from "../components/AdoptThePerfectPetComponent";
 
 test("proparly renders the Alert Component", () => {
