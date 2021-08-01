1ed219fe7e8a21141ff2ab1800866db03de34d4c
Daban <dabansadiq@icloud.com>
2021-08-01 19:00:39 +0300

    testing changes

--- test/AdoptThePerfectPetComponent.test.jsx
+++ test/AdoptThePerfectPetComponent.test.jsx
@@ -3,7 +3,7 @@ import renderer from "react-test-renderer";
 import { test, expect } from "@jest/globals";
 import AdoptThePerfectPetComponent from "../components/AdoptThePerfectPetComponent";
 
-test("proparly renders the Alert Component", () => {
+test("proparly renders the AdoptThePerfectPetComponent component", () => {
   const tree = renderer.create(<AdoptThePerfectPetComponent />).toJSON;
   expect(tree).toMatchSnapshot();
 });
