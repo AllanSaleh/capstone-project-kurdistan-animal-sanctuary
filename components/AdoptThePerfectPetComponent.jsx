49757c7071541d007a39c76a4823b115b420df9f
Ahmad-Fadhel <80587505+Ahmad-Fadhel@users.noreply.github.com>
2021-08-02 12:53:42 +0300

    added internationalization to all components and pages (#60)
    * added internationalization to all components and pages
    
    * finished all the filters in the search filters--no-verify
    
    * added extra options in the filters

--- components/AdoptThePerfectPetComponent.jsx
+++ components/AdoptThePerfectPetComponent.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from "react";
+import { useTranslation } from "next-i18next";
 import AnimalCard from "./AnimalCards";
 
 const AdoptThePerfectPet = () => {
+  const { t } = useTranslation("adoptTheperfectPet");
   const [allValues, setAllValues] = useState({
     petType: "",
     breed: "",
@@ -20,15 +22,15 @@ const AdoptThePerfectPet = () => {
     <div className="flex flex-col w-full">
       <div className="h-40 bg-adopt_perfect_pet_header w-full h-40 flex flex-col justify-center item-center">
         <p className="inline-block w-4/5 text-center w-full text-4xl sm:text-5xl text-white">
-          Adopt the perfect pet
+          {t("adoptTheperfectPet.adopt")}
         </p>
       </div>
       <div className="AdoptThePerfectPet flex flex-row w-full">
         <div className="flex flex-col w-3/12 mx-10">
           <h1 className="mb-5 text-2xl sm:text-3xl text-secondary mt-16">
-            Search Filters
+            {t("adoptTheperfectPet.searchFilters")}
           </h1>
-          <label htmlFor="PetType">PET TYPE</label>
+          <label htmlFor="PetType">{t("adoptTheperfectPet.petType")}</label>
           <input
             type="text"
             id="PetType"
@@ -37,7 +39,7 @@ const AdoptThePerfectPet = () => {
             onChange={searching}
           />
           <label htmlFor="Breed" className="mt-1">
-            BREED
+            {t("adoptTheperfectPet.breed")}
           </label>
           <input
             type="text"
@@ -47,7 +49,7 @@ const AdoptThePerfectPet = () => {
             onChange={searching}
           />
           <label htmlFor="age" className="mt-1">
-            AGE
+            {t("adoptTheperfectPet.age")}
           </label>
           <input
             type="number"
@@ -60,7 +62,7 @@ const AdoptThePerfectPet = () => {
             onChange={searching}
           />
           <label htmlFor="size" className="mt-1">
-            SIZE
+            {t("adoptTheperfectPet.size")}
           </label>
           <select
             type="text"
@@ -69,12 +71,13 @@ const AdoptThePerfectPet = () => {
             className="border-primary border focus:outline-none focus:ring focus:border-primary"
             onChange={searching}
           >
-            <option value="small">small</option>
-            <option value="medium">medium</option>
-            <option value="big">big</option>
+            <option value="">-</option>
+            <option value="small">{t("adoptTheperfectPet.small")}</option>
+            <option value="medium">{t("adoptTheperfectPet.medium")}</option>
+            <option value="big">{t("adoptTheperfectPet.big")}</option>
           </select>
           <label htmlFor="gender" className="mt-1">
-            GENDER
+            {t("adoptTheperfectPet.gender")}
           </label>
           <select
             id="gender"
@@ -82,21 +85,22 @@ const AdoptThePerfectPet = () => {
             className="border-primary border focus:outline-none focus:ring focus:border-primary"
             onChange={searching}
           >
-            <option value="female">female</option>
-            <option value="male">male</option>
+            <option value="">-</option>
+            <option value="female">{t("adoptTheperfectPet.female")}</option>
+            <option value="male">{t("adoptTheperfectPet.male")}</option>
           </select>
-          <label htmlFor="color" className="mt-1">
-            COLOR
+          <label htmlFor="Color" className="mt-1">
+            {t("adoptTheperfectPet.color")}
           </label>
           <input
             type="text"
-            id="color"
+            id="Color"
             name="color"
             className="border-primary border focus:outline-none focus:ring focus:border-primary"
             onChange={searching}
           />
           <label htmlFor="vaccinated" className="mt-1">
-            vaccinated
+            {t("adoptTheperfectPet.vaccinated")}
           </label>
           <select
             id="vaccinated"
@@ -104,8 +108,9 @@ const AdoptThePerfectPet = () => {
             className="border-primary border focus:outline-none focus:ring focus:border-primary"
             onChange={searching}
           >
-            <option value="yes">yes</option>
-            <option value="no">no</option>
+            <option value="">-</option>
+            <option value="yes">{t("adoptTheperfectPet.yes")}</option>
+            <option value="no">{t("adoptTheperfectPet.no")}</option>
           </select>
           <div className="flex justify-center mt-5">
             <img src="./images/Doggo.png" alt="dog" className="w-52 h-52" />
@@ -116,6 +121,10 @@ const AdoptThePerfectPet = () => {
           petType={allValues.petType}
           size={allValues.size}
           gender={allValues.gender}
+          breed={allValues.breed}
+          age={allValues.age}
+          color={allValues.color}
+          vaccinated={allValues.vaccinated}
         />
       </div>
     </div>
