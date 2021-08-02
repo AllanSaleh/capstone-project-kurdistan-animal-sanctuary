7c3d7306f39da398a67da0da76054b3e713bbcf1
Daban <dabansadiq@icloud.com>
2021-08-02 22:33:21 +0300

    Updated the tests

--- test/MobileMenu.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { test, expect } from "@jest/globals";
-import MobileMenu from "../components/MobileMenu";
-
-test("proparly renders the MobileMenu component", () => {
-  const tree = renderer.create(<MobileMenu />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- test/Navbar.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { test, expect } from "@jest/globals";
-import Navbar from "../components/Navbar";
-
-test("proparly renders the Navbar component", () => {
-  const tree = renderer.create(<Navbar />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
