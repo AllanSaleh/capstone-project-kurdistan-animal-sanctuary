812621d01766df34a232177bf38689380a9e0815
Mousa Al-Jawaheri <76170542+mosatii@users.noreply.github.com>
2021-08-02 22:31:52 +0300

    Made some changes in firebase-auth

---
+++ pages/loggedIn.jsx
@@ -0,0 +1,16 @@
+const LoggedIn = () => {
+  const { authUser, loading } = useAuth();
+  const router = useRouter();
+
+  // Listen for changes on loading and authUser, redirect if needed
+  useEffect(() => {
+    if (!loading && !authUser)
+      router.push('/')
+  }, [authUser, loading])
+
+  return (
+    <UserProfile></UserProfile>
+  )
+}
+
+export default LoggedIn;
--- services/firebase.js
+++ services/firebase.js
@@ -7,7 +7,7 @@ const config = {
   projectId: "kurdistan-animal-sanctuary",
   storageBucket: "kurdistan-animal-sanctuary.appspot.com",
   messagingSenderId: "550205779361",
-  appId: "1:550205779361:web:e38c06eebca2543f276fe0",
+  appId: "1:`550205779361:web:e38c06eebca2543f276fe0",
 };
 
 if (!firebase.apps.length) {
